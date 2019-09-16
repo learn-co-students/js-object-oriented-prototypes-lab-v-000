d49b9ab885b6a22fac34cee1dc6a52257ec69535
Nate Dietz <nsdietz@gmail.com>
2019-09-16 13:01:25 -0400

    Done.

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ function BoardMember(name, homeState, training){
 BoardMember.prototype.veto = function(){
 return 'No, I must disagree'
 }
+
 BoardMember.prototype.approve = function(){
 return 'You can do that!'
 }
+
 BoardMember.prototype.doCharity = function(){
 return "I like to help people."
 }
